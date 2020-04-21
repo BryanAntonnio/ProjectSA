f7d99e3534f404e33cb4205a1e69b6b43863bd22
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-04-21 22:35:56 +0700

    Insert account fragment layout and ui java class

---
+++ app/src/main/java/com/uc/finalproject/ui/notes/NotesFragments.java
@@ -0,0 +1,22 @@
+package com.uc.finalproject.ui.notes;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.uc.finalproject.R;
+
+public class NotesFragments extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
+        return view;
+    }
+}
