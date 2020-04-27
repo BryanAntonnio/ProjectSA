0e9fccf16383f45890b73dddd9d842be4ec3243e
BryanAntonnio <benedictbryan744@gmail.com>
2020-04-27 15:57:46 +0700

    uda dikasi frame layout, buat tulisan titlenya blom bagus :)

--- app/src/main/java/com/uc/finalproject/MainActivity.java
+++ app/src/main/java/com/uc/finalproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.uc.finalproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -21,15 +23,20 @@ import com.uc.finalproject.ui.notes.NotesFragments;
 import com.uc.finalproject.ui.reminder.ReminderFragments;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
+    Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar_main);
+        toolbar.setTitle(R.string.notes);
+        setSupportActionBar(toolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setOnNavigationItemSelectedListener(navListener);
 
-        getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_main,
                 new NotesFragments()).commit();
 
     }
@@ -39,24 +46,44 @@ public class MainActivity extends AppCompatActivity {
                 FloatingActionButton button_add_notes;
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                    Fragment selectedFragment = null;
+
 
                     switch (item.getItemId()){
                         case R.id.notes_page:
-                            selectedFragment = new NotesFragments();
-                            break;
+                            toolbar.setTitle(R.string.notes);
+                            setSupportActionBar(toolbar);
+                            fragment = new NotesFragments();
+                            loadFragment(fragment);
+                            return true;
                         case R.id.reminder_page:
-                            selectedFragment = new ReminderFragments();
-                            break;
+                            toolbar.setTitle(R.string.reminder);
+                            setSupportActionBar(toolbar);
+                            fragment = new ReminderFragments();
+                            loadFragment(fragment);
+                            return true;
                         case R.id.account_page:
-                            selectedFragment = new AccountFragments();
-                            break;
-                    }
+                            toolbar.setTitle(R.string.account);
+                            setSupportActionBar(toolbar);
+                            fragment = new AccountFragments();
+                            loadFragment(fragment);
+                            return true;
+                    }return false;
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
-                            selectedFragment).commit();
-
-                    return true;
+//                    getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+//                            selectedFragment).commit();
+//
+//                    return true;
                 }
             };
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_main, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
 }
