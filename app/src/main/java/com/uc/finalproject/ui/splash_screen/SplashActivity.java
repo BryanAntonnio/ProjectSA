56e8f0458cff5204b5d30ae964b6db2d161ad52f
Hans Richard Alim Natadjaja <hansrichard2000@gmail.com>
2020-04-28 21:55:32 +0700

    fix splash screen

--- app/src/main/java/com/uc/finalproject/ui/splash_screen/SplashActivity.java
+++ app/src/main/java/com/uc/finalproject/ui/splash_screen/SplashActivity.java
@@ -3,6 +3,7 @@ package com.uc.finalproject.ui.splash_screen;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -11,20 +12,36 @@ import com.uc.finalproject.R;
 
 public class SplashActivity extends AppCompatActivity {
     private static int splashtime = 4000;
+    SharedPreferences sharedPreferences;
+    Boolean fistTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },splashtime);
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
 
+        fistTime = sharedPreferences.getBoolean("firstTime", true);
+
+        if (fistTime){
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    fistTime = false;
+                    editor.putBoolean("firstTime",fistTime);
+                    editor.apply();
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },splashtime);
+        }
+        else {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
